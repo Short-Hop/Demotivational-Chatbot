470a3155d4b9d90509e9bcbae15b33a38c0f6956
Marion Cointre <marion.cointre@gmail.com>
2019-05-16 14:11:06 -0700

    updated design

--- src/App.js
+++ src/App.js
@@ -1,43 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import axios from 'axios'
-
-
-
-
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
 
 class App extends React.Component {
-  state= {
-    answer: ''
-  }
+  state = {
+    answer: ""
+  };
 
-
-  postMessage= (event) => {
+  postMessage = event => {
     event.preventDefault();
     console.log(event.target.textInput.value);
-    axios.get('http://localhost:8080/?input=' + event.target.textInput.value).then(response => {
-      console.log(response);
-      this.setState({
+    axios
+      .get("http://localhost:8080/?input=" + event.target.textInput.value)
+      .then(response => {
+        console.log(response);
+        this.setState({
+          answer: response.data
+        });
+      });
+  };
 
-        answer: response.data
-      })
-    })
-  }
-  
   render() {
     return (
-    <div>
-      <h1>Demotivator</h1>
-      <h2>{this.state.answer}</h2>
-      <form name="textForm" onSubmit={this.postMessage}>
-        <input type='text' name="textInput"></input>
-        <button>Submit</button>
-      </form>
-    </div>
-  );
+      <div className="main">
+        <div className="title">
+          <h1>Demotivator</h1>
+        </div>
+        <div className="form">
+          <h2>{this.state.answer}</h2>
+          <form name="textForm" onSubmit={this.postMessage}>
+            <input type="text" name="textInput" />
+            <br />
+            <button>Risk it ! </button>
+          </form>
+        </div>
+      </div>
+    );
   }
-  
 }
 
 export default App;
